69aeb7b2a83ea36fef13d32f06e474f33d8ed1b8
Jedd Fenner <jedd@outlook.com>
2021-04-21 16:31:36 +0100

    clear users on deploy to get round permissions errors after redeploy

--- db/seeds/create_users.rb
+++ db/seeds/create_users.rb
@@ -1,13 +1,13 @@
-unless User.exists?(email: "dev@hfeyp.com")
-  User.create! do |u|
-    u.email = "dev@hfeyp.com"
-    u.password = "Partyw0rd"
-    u.role = "editor"
-  end
+User.destroy_all
+ActiveRecord::Base.connection.execute("TRUNCATE users RESTART IDENTITY")
+User.create! do |u|
+  u.email = "cms@cms.gov.uk"
+  u.password = "educationcms"
+  u.role = "editor"
+end
 
-  User.create! do |u|
-    u.email = "guy@home.com"
-    u.password = "guyguy"
-    u.role = "editor"
-  end
+User.create! do |u|
+  u.email = "guy@home.com"
+  u.password = "guyguy"
+  u.role = "editor"
 end
